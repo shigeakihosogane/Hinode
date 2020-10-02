086feccaa5578fb079eb4a626c6d032532f8f75b
shigeakihosogane <xshigeaki@gmail.com>
2020-10-02 18:09:35 +0900

    設定保存機能追加

--- FaxTransfer/FaxTransfer/Form1.Designer.vb
+++ FaxTransfer/FaxTransfer/Form1.Designer.vb
@@ -31,36 +31,36 @@ Partial Class Form1
         Me.btn参照1 = New System.Windows.Forms.Button()
         Me.btn参照2 = New System.Windows.Forms.Button()
         Me.btn参照3 = New System.Windows.Forms.Button()
-        Me.btn設定保存 = New System.Windows.Forms.Button()
         Me.lblステータス = New System.Windows.Forms.Label()
         Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnテスト = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'txt転送元
         '
-        Me.txt転送元.Location = New System.Drawing.Point(197, 215)
+        Me.txt転送元.Location = New System.Drawing.Point(108, 212)
         Me.txt転送元.Name = "txt転送元"
-        Me.txt転送元.Size = New System.Drawing.Size(464, 19)
+        Me.txt転送元.Size = New System.Drawing.Size(313, 19)
         Me.txt転送元.TabIndex = 0
         '
         'txt監視
         '
-        Me.txt監視.Location = New System.Drawing.Point(197, 282)
+        Me.txt監視.Location = New System.Drawing.Point(108, 279)
         Me.txt監視.Name = "txt監視"
-        Me.txt監視.Size = New System.Drawing.Size(464, 19)
+        Me.txt監視.Size = New System.Drawing.Size(313, 19)
         Me.txt監視.TabIndex = 1
         '
         'txt転送先
         '
-        Me.txt転送先.Location = New System.Drawing.Point(197, 339)
+        Me.txt転送先.Location = New System.Drawing.Point(108, 336)
         Me.txt転送先.Name = "txt転送先"
-        Me.txt転送先.Size = New System.Drawing.Size(464, 19)
+        Me.txt転送先.Size = New System.Drawing.Size(313, 19)
         Me.txt転送先.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(59, 218)
+        Me.Label1.Location = New System.Drawing.Point(26, 215)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(76, 12)
         Me.Label1.TabIndex = 3
@@ -69,7 +69,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(59, 285)
+        Me.Label2.Location = New System.Drawing.Point(38, 282)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(64, 12)
         Me.Label2.TabIndex = 4
@@ -78,7 +78,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(59, 342)
+        Me.Label3.Location = New System.Drawing.Point(26, 339)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(76, 12)
         Me.Label3.TabIndex = 5
@@ -86,7 +86,7 @@ Partial Class Form1
         '
         'btn参照1
         '
-        Me.btn参照1.Location = New System.Drawing.Point(683, 213)
+        Me.btn参照1.Location = New System.Drawing.Point(436, 210)
         Me.btn参照1.Name = "btn参照1"
         Me.btn参照1.Size = New System.Drawing.Size(75, 23)
         Me.btn参照1.TabIndex = 6
@@ -95,7 +95,7 @@ Partial Class Form1
         '
         'btn参照2
         '
-        Me.btn参照2.Location = New System.Drawing.Point(683, 280)
+        Me.btn参照2.Location = New System.Drawing.Point(436, 277)
         Me.btn参照2.Name = "btn参照2"
         Me.btn参照2.Size = New System.Drawing.Size(75, 23)
         Me.btn参照2.TabIndex = 7
@@ -104,21 +104,12 @@ Partial Class Form1
         '
         'btn参照3
         '
-        Me.btn参照3.Location = New System.Drawing.Point(683, 337)
+        Me.btn参照3.Location = New System.Drawing.Point(436, 334)
         Me.btn参照3.Name = "btn参照3"
         Me.btn参照3.Size = New System.Drawing.Size(75, 23)
         Me.btn参照3.TabIndex = 8
         Me.btn参照3.Text = "参照"
         Me.btn参照3.UseVisualStyleBackColor = True
-        '
-        'btn設定保存
-        '
-        Me.btn設定保存.Location = New System.Drawing.Point(683, 403)
-        Me.btn設定保存.Name = "btn設定保存"
-        Me.btn設定保存.Size = New System.Drawing.Size(75, 23)
-        Me.btn設定保存.TabIndex = 9
-        Me.btn設定保存.Text = "設定を保存"
-        Me.btn設定保存.UseVisualStyleBackColor = True
         '
         'lblステータス
         '
@@ -126,28 +117,38 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.lblステータス.AutoSize = True
         Me.lblステータス.Font = New System.Drawing.Font("MS UI Gothic", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.lblステータス.Location = New System.Drawing.Point(250, 44)
+        Me.lblステータス.Location = New System.Drawing.Point(91, 41)
         Me.lblステータス.Name = "lblステータス"
-        Me.lblステータス.Size = New System.Drawing.Size(0, 97)
+        Me.lblステータス.Size = New System.Drawing.Size(330, 97)
         Me.lblステータス.TabIndex = 10
+        Me.lblステータス.Text = "停止中"
         '
         'btn1
         '
-        Me.btn1.Location = New System.Drawing.Point(683, 118)
+        Me.btn1.Location = New System.Drawing.Point(436, 109)
         Me.btn1.Name = "btn1"
         Me.btn1.Size = New System.Drawing.Size(75, 23)
         Me.btn1.TabIndex = 11
-        Me.btn1.Text = "Button1"
+        Me.btn1.Text = "開始"
         Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btnテスト
+        '
+        Me.btnテスト.Location = New System.Drawing.Point(436, 396)
+        Me.btnテスト.Name = "btnテスト"
+        Me.btnテスト.Size = New System.Drawing.Size(75, 23)
+        Me.btnテスト.TabIndex = 12
+        Me.btnテスト.Text = "テスト"
+        Me.btnテスト.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(546, 450)
+        Me.Controls.Add(Me.btnテスト)
         Me.Controls.Add(Me.btn1)
         Me.Controls.Add(Me.lblステータス)
-        Me.Controls.Add(Me.btn設定保存)
         Me.Controls.Add(Me.btn参照3)
         Me.Controls.Add(Me.btn参照2)
         Me.Controls.Add(Me.btn参照1)
@@ -173,7 +174,7 @@ Partial Class Form1
     Friend WithEvents btn参照1 As Button
     Friend WithEvents btn参照2 As Button
     Friend WithEvents btn参照3 As Button
-    Friend WithEvents btn設定保存 As Button
     Friend WithEvents lblステータス As Label
     Friend WithEvents btn1 As Button
+    Friend WithEvents btnテスト As Button
 End Class
--- FaxTransfer/FaxTransfer/Form1.vb
+++ FaxTransfer/FaxTransfer/Form1.vb
@@ -1,6 +1,11 @@
 ﻿Imports System
 Imports System.IO
 Imports System.Text
+Imports System.Runtime.Serialization
+Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.Windows.Forms
+Imports Microsoft.Win32
+
 
 Public Class Form1
 
@@ -11,17 +16,19 @@ Public Class Form1
     Dim tennsousaki As String
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MainClass.設定読込()
 
-        lblステータス.Text = "停止中"
-        btn1.Text = "開始"
-
-
-        txt転送元.Text = "\\192.168.2.240\fax受信"
-        txt監視.Text = "\\192.168.2.240\fax転送"
-        txt転送先.Text = "\\192.168.2.240\ThereforeDATA"
-
+        'txt転送元.Text = "\\192.168.2.240\fax受信"
+        'txt監視.Text = "\\192.168.2.240\fax転送"
+        'txt転送先.Text = "\\192.168.2.240\ThereforeDATA"
 
     End Sub
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        MainClass.設定保存()
+
+    End Sub
+
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
 
         If lblステータス.Text = "停止中" Then
@@ -98,176 +105,154 @@ Public Class Form1
                 Dim fname As String = Path.GetFileNameWithoutExtension(e.FullPath)
                 Dim fnarray As Array = Split(fname, "_")
 
-                If fnarray(0) <> "" Then
+                Dim flg As Boolean = True
+                Dim errorstr As String = ""
 
-                    Dim zyutyuuCD As Decimal = CDec(fnarray(3))
+                If IsNumeric(fnarray(0)) Then '-数値
 
-                    Dim cnstr As String = "Data Source=SV201710;Initial Catalog=HINODEDB;USER ID=sa;PASSWORD=cube%6614;"
-                    Dim sqlstr As String = "SELECT 開始日, 終了日, 担当部署, 備考 FROM T_TF_D_index WHERE 受注ID = " & zyutyuuCD
+                    Try
 
+                        Dim zyutyuuCD As Decimal = CDec(fnarray(0))
 
-                    Dim cn As SqlClient.SqlConnection
-                    Dim cmd As SqlClient.SqlCommand
-                    Dim reader As SqlClient.SqlDataReader
-
-                    Dim sdate As Date
-                    Dim edate As Date
-                    Dim busyo As String
-                    Dim bikou As String
-
-                    cn = New SqlClient.SqlConnection(cnstr)
-
-                    cmd = cn.CreateCommand
-                    cmd.CommandText = sqlstr
-
-                    cn.Open()
-                    Console.WriteLine(cn.State)
-
-                    reader = cmd.ExecuteReader()
-
-                    Do While reader.Read()
-                        sdate = reader("開始日")
-                        edate = reader("終了日")
-                        busyo = reader("担当部署")
-                        bikou = reader("備考")
-                    Loop
-
-                    fname = fnarray(1)
-                    fname = fname & "_" & fnarray(2)
-                    fname = fname & "_" & fnarray(3)
-                    fname = fname & "_" & fnarray(0)
-                    fname = fname & "_" & Format(sdate, "yyyymmdd")
-                    fname = fname & "_" & Format(edate, "yyyymmdd")
-                    fname = fname & "_" & busyo
-                    fname = fname & "_" & bikou
-                    fname = fname & "_" & ".pdf"
+                        Dim cnstr As String = "Data Source=SV201710;Initial Catalog=HINODEDB;USER ID=sa;PASSWORD=cube%6614;"
+                        Dim sqlstr As String = "SELECT 開始日, 終了日, 担当部署, 備考 FROM T_TF_D_index WHERE 受注ID = " & zyutyuuCD
 
 
-                    reader.Close()
-                    cmd.Dispose()
-                    cn.Close()
-                    cn.Dispose()
+                        Dim cn As SqlClient.SqlConnection
+                        Dim cmd As SqlClient.SqlCommand
+                        Dim reader As SqlClient.SqlDataReader
 
-                    Dim tennsousakipath As String = tennsousaki
+                        Dim sdate As Date
+                        Dim edate As Date
+                        Dim busyo As String
+                        Dim bikou As String
 
-                    System.IO.File.Move(e.FullPath, tennsousakipath & "\" & fname)
+                        cn = New SqlClient.SqlConnection(cnstr)
+
+                        cmd = cn.CreateCommand
+                        cmd.CommandText = sqlstr
+
+                        cn.Open()
+                        Console.WriteLine(cn.State)
+
+                        reader = cmd.ExecuteReader()
+
+                        If reader.HasRows = True Then '---レコードあり
+
+                            Do While reader.Read()
+                                sdate = reader("開始日")
+                                edate = reader("終了日")
+                                busyo = reader("担当部署")
+                                bikou = reader("備考")
+                            Loop
+
+                            fname = fnarray(1)
+                            fname = fname & "_" & fnarray(2)
+                            fname = fname & "_" & fnarray(3)
+                            fname = fname & "_" & fnarray(0)
+                            fname = fname & "_" & Format(sdate, "yyyymmdd")
+                            fname = fname & "_" & Format(edate, "yyyymmdd")
+                            fname = fname & "_" & busyo
+                            fname = fname & "_" & bikou
+                            fname = fname & "_" & ".pdf"
 
 
-                    Console.WriteLine(fname)
+                            reader.Close()
+                            cmd.Dispose()
+                            cn.Close()
+                            cn.Dispose()
+
+
+                            System.IO.File.Copy(e.FullPath, tennsousaki & "\" & fname, True)
+                            System.IO.File.Delete(e.FullPath)
+
+                            'Console.WriteLine(fname)
+
+
+
+
+                        Else '--------------------------レコードなし
+
+                            flg = False
+                            errorstr = "受注CD該当なし"
+
+                        End If
+
+
+                    Catch ex As System.IO.FileNotFoundException '例外が発生
+
+                        flg = False
+                        errorstr = ex.Message
+
+                    End Try
+
+                Else '-数値以外
+
+                    flg = False
+                    errorstr = "受注CD不正"
 
                 End If
+
+                If flg = False Then
+
+                    fname = errorstr
+                    fname = fname & "_" & fnarray(1)
+                    fname = fname & "_" & fnarray(2)
+                    fname = fname & "_" & fnarray(3)
+
+                    System.IO.File.Copy(e.FullPath, tennsoumoto & "\" & fname & ".pdf", True)
+                    System.IO.File.Delete(e.FullPath)
+
+                End If
+
+
 
         End Select
 
     End Sub
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     Private Sub btn参照1_Click(sender As Object, e As EventArgs) Handles btn参照1.Click
         Dim fbd As New FolderBrowserDialog
-        '上部に表示する説明テキストを指定する
         fbd.Description = "フォルダを指定してください。"
-        'ルートフォルダを指定する
-        'デフォルトでDesktop
         fbd.RootFolder = Environment.SpecialFolder.Desktop
-        '最初に選択するフォルダを指定する
-        'RootFolder以下にあるフォルダである必要がある
         fbd.SelectedPath = "C:\Windows"
-        'ユーザーが新しいフォルダを作成できるようにする
-        'デフォルトでTrue
         fbd.ShowNewFolderButton = True
 
-        'ダイアログを表示する
         If fbd.ShowDialog(Me) = DialogResult.OK Then
-            '選択されたフォルダを表示する
             txt転送元.Text = fbd.SelectedPath
         End If
     End Sub
 
     Private Sub btn参照2_Click(sender As Object, e As EventArgs) Handles btn参照2.Click
         Dim fbd As New FolderBrowserDialog
-        '上部に表示する説明テキストを指定する
         fbd.Description = "フォルダを指定してください。"
-        'ルートフォルダを指定する
-        'デフォルトでDesktop
         fbd.RootFolder = Environment.SpecialFolder.Desktop
-        '最初に選択するフォルダを指定する
-        'RootFolder以下にあるフォルダである必要がある
         fbd.SelectedPath = "C:\Windows"
-        'ユーザーが新しいフォルダを作成できるようにする
-        'デフォルトでTrue
         fbd.ShowNewFolderButton = True
 
-        'ダイアログを表示する
         If fbd.ShowDialog(Me) = DialogResult.OK Then
-            '選択されたフォルダを表示する
             txt監視.Text = fbd.SelectedPath
         End If
     End Sub
 
     Private Sub btn参照3_Click(sender As Object, e As EventArgs) Handles btn参照3.Click
         Dim fbd As New FolderBrowserDialog
-        '上部に表示する説明テキストを指定する
         fbd.Description = "フォルダを指定してください。"
-        'ルートフォルダを指定する
-        'デフォルトでDesktop
         fbd.RootFolder = Environment.SpecialFolder.Desktop
-        '最初に選択するフォルダを指定する
-        'RootFolder以下にあるフォルダである必要がある
         fbd.SelectedPath = "C:\Windows"
-        'ユーザーが新しいフォルダを作成できるようにする
-        'デフォルトでTrue
         fbd.ShowNewFolderButton = True
 
-        'ダイアログを表示する
         If fbd.ShowDialog(Me) = DialogResult.OK Then
-            '選択されたフォルダを表示する
             txt転送先.Text = fbd.SelectedPath
         End If
     End Sub
 
-    Private Sub btn設定保存_Click(sender As Object, e As EventArgs) Handles btn設定保存.Click
+
+    Private Sub btnテスト_Click(sender As Object, e As EventArgs) Handles btnテスト.Click
+
+
+
+
 
 
 
@@ -275,3 +260,64 @@ Public Class Form1
 
 
 End Class
+
+'XMLファイルに保存するオブジェクトのためのクラス
+Public Class SampleClass
+    Public tennsoumoto As String
+    Public kannsi As String
+    Public tennsousaki As String
+End Class
+
+Class MainClass
+    'SampleClassオブジェクトをXMLファイルに保存する
+    Public Shared Sub 設定保存()
+        '保存先のファイル名
+        Dim strPath As String = ""
+        strPath = System.Reflection.Assembly.GetExecutingAssembly().Location
+        strPath = System.IO.Path.GetDirectoryName(strPath)
+        strPath = strPath & "\config.xml"
+
+        '保存するクラス(SampleClass)のインスタンスを作成
+        Dim obj As New SampleClass()
+        obj.tennsoumoto = Form1.txt転送元.Text
+        obj.kannsi = Form1.txt監視.Text
+        obj.tennsousaki = Form1.txt転送先.Text
+
+        'XmlSerializerオブジェクトを作成
+        'オブジェクトの型を指定する
+        Dim serializer As New System.Xml.Serialization.XmlSerializer(GetType(SampleClass))
+        '書き込むファイルを開く（UTF-8 BOM無し）
+        Dim sw As New System.IO.StreamWriter(strPath, False, New System.Text.UTF8Encoding(False))
+        'シリアル化し、XMLファイルに保存する
+        serializer.Serialize(sw, obj)
+        'ファイルを閉じる
+        sw.Close()
+    End Sub
+
+    'XMLファイルをSampleClassオブジェクトに復元する
+    Public Shared Sub 設定読込()
+        '保存元のファイル名
+        Dim strPath As String = ""
+        strPath = System.Reflection.Assembly.GetExecutingAssembly().Location
+        strPath = System.IO.Path.GetDirectoryName(strPath)
+        strPath = strPath & "\config.xml"
+
+        'XmlSerializerオブジェクトを作成
+        Dim serializer As New System.Xml.Serialization.XmlSerializer(GetType(SampleClass))
+        '読み込むファイルを開く
+        Dim sr As New System.IO.StreamReader(strPath, New System.Text.UTF8Encoding(False))
+        'XMLファイルから読み込み、逆シリアル化する
+        Dim obj As SampleClass = DirectCast(serializer.Deserialize(sr), SampleClass)
+
+        Form1.txt転送元.Text = obj.tennsoumoto
+        Form1.txt監視.Text = obj.kannsi
+        Form1.txt転送先.Text = obj.tennsousaki
+
+        'ファイルを閉じる
+        sr.Close()
+
+    End Sub
+
+
+End Class
+
