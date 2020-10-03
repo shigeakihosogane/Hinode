c8dd425ed10d37230520d6dfe2e8da78af44f661
shigeakihosogane <xshigeaki@gmail.com>
2020-10-03 15:10:22 +0900

    削除をやめてエラー回避した

--- FaxTransfer/FaxTransfer/Form1.vb
+++ FaxTransfer/FaxTransfer/Form1.vb
@@ -37,6 +37,7 @@ Public Class Form1
                 lblステータス.Text = "稼働中"
                 btn1.Text = "停止"
                 転送開始()
+                '監視開始()
             Else
                 MsgBox("フォルダを選択してください。")
             End If
@@ -44,6 +45,7 @@ Public Class Form1
             lblステータス.Text = "停止中"
             btn1.Text = "開始"
             転送停止()
+            '監視停止()
         End If
 
     End Sub
@@ -64,12 +66,11 @@ Public Class Form1
         watcher.Path = kannsi
 
         '最終アクセス日時、最終更新日時、ファイル、フォルダ名の変更を監視する
-        watcher.NotifyFilter = System.IO.NotifyFilters.LastAccess Or
-            System.IO.NotifyFilters.LastWrite Or
-            System.IO.NotifyFilters.FileName Or
-            System.IO.NotifyFilters.DirectoryName
+        watcher.NotifyFilter = System.IO.NotifyFilters.FileName
+
         'すべてのファイルを監視
         watcher.Filter = ""
+
         'UIのスレッドにマーシャリングする
         'コンソールアプリケーションでの使用では必要ない
         watcher.SynchronizingObject = Me
@@ -118,97 +119,93 @@ Public Class Form1
         fname = Replace(fname, Space(1), String.Empty)
         Dim fnarray As Array = Split(fname, "_")
 
-        Console.WriteLine(fname)
-
         Dim flg As Boolean = True
         Dim errorstr As String = ""
 
         If IsNumeric(fnarray(0)) Then '-数値
 
-            Try
+            Dim zyutyuuCD As Decimal = CDec(fnarray(0))
 
-                Dim zyutyuuCD As Decimal = CDec(fnarray(0))
-
-                Dim cnstr As String = "Data Source=SV201710;Initial Catalog=HINODEDB;USER ID=sa;PASSWORD=cube%6614;"
-                Dim sqlstr As String = "SELECT 開始日, 終了日, 担当部署, 備考 FROM T_TF_D_index WHERE 受注ID = " & zyutyuuCD
+            Dim cnstr As String = "Data Source=SV201710;Initial Catalog=HINODEDB;USER ID=sa;PASSWORD=cube%6614;"
+            Dim sqlstr As String = "SELECT 開始日, 終了日, 担当部署, 備考 FROM T_TF_D_index WHERE 受注ID = " & zyutyuuCD
 
 
-                Dim cn As SqlClient.SqlConnection
-                Dim cmd As SqlClient.SqlCommand
-                Dim reader As SqlClient.SqlDataReader
+            Dim cn As SqlClient.SqlConnection
+            Dim cmd As SqlClient.SqlCommand
+            Dim reader As SqlClient.SqlDataReader
 
-                Dim sdate As Date
-                Dim edate As Date
-                Dim busyo As String = ""
-                Dim bikou As String = ""
+            Dim sdate As Date
+            Dim edate As Date
+            Dim busyo As String = ""
+            Dim bikou As String = ""
 
-                cn = New SqlClient.SqlConnection(cnstr)
+            cn = New SqlClient.SqlConnection(cnstr)
 
-                cmd = cn.CreateCommand
-                cmd.CommandText = sqlstr
+            cmd = cn.CreateCommand
+            cmd.CommandText = sqlstr
 
-                cn.Open()
-                'Console.WriteLine(cn.State)
+            cn.Open()
+            'Console.WriteLine(cn.State)
 
-                reader = cmd.ExecuteReader()
+            reader = cmd.ExecuteReader()
 
-                If reader.HasRows = True Then '---レコードあり
+            If reader.HasRows = True Then '---レコードあり
 
-                    Do While reader.Read()
-                        sdate = reader("開始日")
-                        edate = reader("終了日")
-                        busyo = reader("担当部署")
-                        bikou = reader("備考")
-                    Loop
+                Do While reader.Read()
+                    sdate = reader("開始日")
+                    edate = reader("終了日")
+                    busyo = reader("担当部署")
+                    bikou = reader("備考")
+                Loop
 
-                    fname = fnarray(1)
-                    fname = fname & "_" & fnarray(2)
-                    fname = fname & "_" & fnarray(3)
-                    fname = fname & "_" & fnarray(0)
-                    fname = fname & "_" & Format(sdate, "yyyymmdd")
-                    fname = fname & "_" & Format(edate, "yyyymmdd")
-                    fname = fname & "_" & busyo
-                    fname = fname & "_" & bikou
-                    If fnarray.Length = 4 Then
-                        fname = fname & "_1"
-                    Else
-                        fname = fname & "_" & fnarray(4)
-                    End If
-                    fname = fname & ".pdf"
-
-
-                    reader.Close()
-                    cmd.Dispose()
-                    cn.Close()
-                    cn.Dispose()
-
-
-
-                    System.IO.File.Copy(e.FullPath, tennsousaki & "\" & fname, True)
-                    System.IO.File.Delete(e.FullPath)
-
-                    'Console.WriteLine(fname)
-
-
-                Else '--------------------------レコードなし
-
-                    flg = False
-                    errorstr = "受注CD該当なし"
-
+                fname = fnarray(1)
+                fname = fname & "_" & fnarray(2)
+                fname = fname & "_" & fnarray(3)
+                fname = fname & "_" & fnarray(0)
+                fname = fname & "_" & Format(sdate, "yyyymmdd")
+                fname = fname & "_" & Format(edate, "yyyymmdd")
+                fname = fname & "_" & busyo
+                fname = fname & "_" & bikou
+                If fnarray.Length = 4 Then
+                    fname = fname & "_1"
+                Else
+                    fname = fname & "_" & fnarray(4)
                 End If
+                fname = fname & ".pdf"
 
 
-            Catch ex As System.IO.FileNotFoundException
+                reader.Close()
+                cmd.Dispose()
+                cn.Close()
+                cn.Dispose()
+
+                Try
+                    System.IO.File.Copy(e.FullPath, tennsousaki & "\" & fname & ".pdf", True)
+                    'Console.WriteLine(fname)
+                Catch ex As System.IO.FileNotFoundException 'コピーするにファイルがない場合
+                    flg = False
+                    errorstr = ex.Message
+                Catch ex As System.IO.DirectoryNotFoundException 'コピー先のファルダが存在しない
+                    flg = False
+                    errorstr = ex.Message
+                Catch ex As System.IO.IOException 'コピー先のファイルがすでに存在している場合
+                    flg = False
+                    errorstr = ex.Message
+                Catch ex As System.UnauthorizedAccessException 'コピー先のファイルへのアクセスが拒否された場合
+                    flg = False
+                    errorstr = ex.Message
+                End Try
+
+                'MsgBox(fname)
+
+            Else '--------------------------レコードなし
 
                 flg = False
-                errorstr = ex.Message
+                errorstr = "受注CD該当なし"
 
-            Catch ex As System.IndexOutOfRangeException
+            End If
 
-                flg = False
-                errorstr = ex.Message
 
-            End Try
 
         Else '-数値以外
 
@@ -216,6 +213,7 @@ Public Class Form1
             errorstr = "受注CD不正"
 
         End If
+
 
         If flg = False Then
 
@@ -230,15 +228,16 @@ Public Class Form1
             End If
             fname = fname & ".pdf"
 
-            'System.IO.File.Move(e.FullPath, tennsoumoto & "\" & fname & ".pdf")
             System.IO.File.Copy(e.FullPath, tennsoumoto & "\" & fname & ".pdf", False)
-            System.IO.File.Delete(e.FullPath)
 
         End If
 
 
+        'System.IO.File.Delete(e.FullPath)
+
 
     End Sub
+
 
     Private Sub btn参照1_Click(sender As Object, e As EventArgs) Handles btn参照1.Click
         Dim fbd As New FolderBrowserDialog
@@ -276,11 +275,68 @@ Public Class Form1
         End If
     End Sub
 
+    Private Sub 監視開始()
+
+
+        Dim watcher As New System.IO.FileSystemWatcher
+        '監視するディレクトリを指定
+        watcher.Path = txt監視.Text
+        '*.txtファイルを監視、すべて監視するときは""にする
+        watcher.Filter = ""
+        'ファイル名とディレクトリ名と最終書き込む日時の変更を監視
+        watcher.NotifyFilter = System.IO.NotifyFilters.FileName
+        'サブディレクトリは監視しない
+        watcher.IncludeSubdirectories = False
+        '必要に応じてバッファサイズを変更
+        'watcher.InternalBufferSize = 4096
+        '同期的に監視を開始する
+        Dim changedResult As System.IO.WaitForChangedResult = watcher.WaitForChanged(System.IO.WatcherChangeTypes.Renamed)
+
+        MsgBox(changedResult.Name)
+
+        Console.WriteLine(changedResult.Name)
+
+
+
+        '===========================================================================================================================================
+
+
+        If changedResult.TimedOut Then
+            Console.WriteLine("タイムアウトしました。")
+            Return
+        End If
+
+        '変更があったときに結果を表示する
+        Select Case changedResult.ChangeType
+            Case System.IO.WatcherChangeTypes.Renamed
+                Console.WriteLine(("ファイル 「" +
+                    changedResult.OldName + "」の名前が「" +
+                    changedResult.Name + "」に変更されました。"))
+            Case System.IO.WatcherChangeTypes.Changed
+                Console.WriteLine(("ファイル 「" +
+                    changedResult.Name + "」が変更されました。"))
+            Case System.IO.WatcherChangeTypes.Created
+                Console.WriteLine(("ファイル 「" +
+                    changedResult.Name + "」が作成されました。"))
+            Case System.IO.WatcherChangeTypes.Deleted
+                Console.WriteLine(("ファイル 「" +
+                    changedResult.Name + "」が削除されました。"))
+
+        End Select
+
+    End Sub
+
+    Private Sub 監視停止()
+
+        'watcher.EnableRaisingEvents = False
+        'watcher.Dispose()
+        'watcher = Nothing
+
+    End Sub
+
+
 
     Private Sub btnテスト_Click(sender As Object, e As EventArgs) Handles btnテスト.Click
-
-
-
 
 
 
