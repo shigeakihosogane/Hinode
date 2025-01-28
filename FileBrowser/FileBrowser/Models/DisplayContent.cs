b2e8961534bdd1497a50823530e030db1016a9ea
Gane <xshigeaki@gmail.com>
2025-01-28 16:22:09 +0900

    20250127_フォルダ選択後内容をListビューに出力できた

---
+++ FileBrowser/FileBrowser/Models/DisplayContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBrowser.Models
+{
+    public class DisplayContent
+    {        
+        public string Name { get; set; } = "";
+        public bool IsFolder { get; set; } = false;
+        public string FullPath { get; set; } = "";
+        public string NetworkPath { get; set; } = "";
+
+    }
+}
